7907d1a660eced7e1e238fd2c3337edf8737b16b
“ahadvt” <ahadvt@code.edu.az>
2022-10-26 17:22:49 +0400

    done

---
+++ Telegram/Telegram/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using System.IO;
+
+
+
+
+TelegramBotClient Bot = new TelegramBotClient("xxxxxx");
+
+ void Form1_Load(object sender, EventArgs e)
+{
+    Bot.StartReceiving();
+    Bot.OnMessage += Bot_OnMessage;
+}
+
+
